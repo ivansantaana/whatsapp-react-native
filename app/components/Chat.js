9d548021998a46f451616246f32147aacdc990fe
Ivan <ivansantanacalle@gmail.com>
2021-10-21 12:35:14 +0200

    Created the data.js file saving all the URLs of the different APIs (calls, chats and states). Installed the axios package.

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {View, Text} from "react-native";
+import axios from "axios";
+import { FAKE_CHATS } from "../data/data";
 
 export default class Chat extends Component {
     render() {
