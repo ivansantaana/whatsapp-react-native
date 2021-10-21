a939b4dfe2b511db2a1f1b2ce1e98f960fd1b994
Iván Santana Calle <ivansantanacalle@gmail.com>
2021-10-21 07:22:13 +0000

    We add styles before start with the structure of the App file

--- App.js
+++ App.js
@@ -10,3 +10,37 @@ export default class App extends Component{
     )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer:{
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyCOntent: "space-between",
+    backgroundColor: "#075e54",
+    alignItems: "center"
+  },
+  contentContainer: {
+    flex: 8
+  },
+  rightHeaderContainer: {
+    flexDirection: "row",
+    alignItems: "flex-end"
+  },
+  leftHeaderContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start"
+  },
+  logo: {
+    color: "#ffffff",
+    fontSize: 18,
+    marginLeft: 10,
+    fontWeight: "bold"
+  },
+  icon: {
+    padding: 5,
+  }
+});
