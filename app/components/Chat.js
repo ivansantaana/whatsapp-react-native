364731b45636c2cdef81653ae660cc99e787db94
Ivan <ivansantanacalle@gmail.com>
2021-10-22 10:35:44 +0200

    Create constructor and initialize state. Create componentDidMount method to perform http requests. Import the ActivityIndicator component to render a load spinner and the FlatList component to render it. Render the list and if the status is not true show the spinner.

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,12 +1,41 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {View, Text, FlatList, ActivityIndicator} from "react-native";
 import axios from "axios";
 import { FAKE_CHATS } from "../data/data";
 
-export default class Chat extends Component {
+export default class Chat extends Component {   
+    constructor(props) {
+        super(props)
+        this.state = {
+            chatList : [],
+            loaded: false
+        }
+    }
+    componentDidMount() {
+        axios.get(FAKE_CHATS)
+        .then((response) => {
+            this.setState({
+                chatList: response.data,
+                loaded: true
+            })
+        })
+       .catch(function(error) {
+           console.log(error);
+       });
+    }
     render() {
-        return (
-            <View><Text>Chat</Text></View>
-        );
+        if(this.state.loaded) {
+            return (
+                <FlatList
+                    data={this.state.chatList}
+                    renderItem={({item}) => <Text>{item.first_name}</Text>}
+                    keyExtractor={item => item.id}
+                />
+            )
+        } else {
+            return(
+                <ActivityIndicator size="large" />
+            )
+        }
     }
 }
