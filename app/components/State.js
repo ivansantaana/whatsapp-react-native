f5c76a135ee5ec8f98e7747e3f766d5467017732
Ivan <ivansantanacalle@gmail.com>
2021-10-25 10:08:31 +0200

    Added paragraph my state within state. It remains to fix the Recents container.

--- app/components/State.js
+++ app/components/State.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {FlatList, ActivityIndicator} from "react-native";
+import {View, Text, FlatList, ActivityIndicator, Image, StyleSheet} from "react-native";
 import axios from "axios";
 import { FAKE_STATES } from "../data/data";
 import ListState from "./ListState";
@@ -27,6 +27,33 @@ export default class State extends Component {
     render() {
         if(this.state.loaded) {
             return (
+                <>
+                <View style = {styles.listItemContainer}>
+                <View style = {styles.avatarContainer}>
+                    <Image
+                        style={styles.avatar}
+                        source={{uri: "https://cdn-icons-png.flaticon.com/512/581/581707.png"}}
+                    />
+                </View>
+                <View style = {styles.stateDetailsContainer}>
+                    <View style={styles.stateDetailsContainerWrap}>
+                        <View style={styles.nameContainer}>
+                            <Text style={styles.nameText}>{"Mi estado"}</Text>
+                        </View>
+                    </View>
+                <View style={styles.stateDetailsContainerWrap}>
+                    <View style={styles.date_timeContainer}>
+                        <Text style = {styles.date_timeContainer}>{"añade una actualización"}</Text>
+                    </View>
+                </View>
+                </View>
+            </View>
+            <View style ={styles.grey}>
+                <View style = {styles.nameContainer}>
+                    <Text style = {styles.recientes}>{"Recientes"}</Text>
+                </View>
+            </View>
+            
                 <FlatList
                     data={this.state.stateList}
                     renderItem={({item}) => (
@@ -39,6 +66,7 @@ export default class State extends Component {
                     )}
                     keyExtractor={item => item.id}
                 />
+                </>
             )
         } else {
             return(
@@ -47,3 +75,69 @@ export default class State extends Component {
         }
     }
 }
+
+const styles = StyleSheet.create({
+    listItemContainer: {
+        flex: 1,
+        flexDirection: "row",
+        padding:10
+    },
+    avatarContainer: {
+        flex: 1,
+        alignItems: "flex-start"
+    },
+    stateDetailsContainer: {
+        flex: 4,
+        borderBottomColor: "rgba(92,94,94,0.5)",
+        borderBottomWidth: 0.25
+    },
+    stateDetailsContainerWrap: {
+        flex: 1,
+        flexDirection: "row",
+        padding: 5
+    },
+    nameContainer: {
+        alignItems: "flex-start",
+        flex: 1
+    },
+    dateContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems:"flex-end"
+    },
+    date_timeContainer: {
+        flex: 1
+    },
+    nameText: {
+        fontWeight: "bold",
+        color: "#000"
+    },
+    dateText: {
+        fontSize: 12
+    },
+    avatar: {
+        borderRadius: 30,
+        width: 60,
+        height: 60
+    },
+    video_callContainer: {
+        flex: 1,
+        alignItems: "flex-end"
+    },
+    video_call: {
+        borderRadius: 30,
+        width: 30,
+        height: 30
+    },
+    grey:{
+        flex: 1,
+        flexDirection: "row",
+        padding: 5,
+        height: 10,
+        backgroundColor: "#FFFFFF"
+    },
+    recientes: {
+        fontWeight: "bold",
+        color: "#008F39"
+    }
+});
